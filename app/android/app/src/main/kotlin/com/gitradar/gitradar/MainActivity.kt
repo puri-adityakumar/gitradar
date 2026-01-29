d4fd3b929955df72580868c88972702ab2a2eb37
puri-adityakumar <kumar.adityapuri@gmail.com>
2026-01-30 03:20:48 +0530

    minor fix

---
+++ app/android/app/src/main/kotlin/com/gitradar/gitradar/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.gitradar.gitradar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ app/android/settings.gradle.kts
@@ -0,0 +1,28 @@
+pluginManagement {
+    val flutterSdkPath = run {
+        val properties = java.util.Properties()
+        val localPropertiesFile = file("local.properties")
+        if (localPropertiesFile.exists()) {
+            localPropertiesFile.inputStream().use { properties.load(it) }
+        }
+        properties.getProperty("flutter.sdk")
+            ?: System.getenv("FLUTTER_ROOT")
+            ?: error("flutter.sdk not set in local.properties and FLUTTER_ROOT env var not set")
+    }
+
+    includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
+
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+plugins {
+    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
+    id("com.android.application") version "8.11.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
+}
+
+include(":app")
